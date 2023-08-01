6a04574bf335aa85481f948f240b716588698110
casmeyu <martin@casamayou.net>
2023-08-01 11:50:48 -0300

    Add: user password is now hashed using bcrypt when creating a user. TODO: login for the user and add JWT token authentication

--- models/user.go
+++ models/user.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -23,11 +24,16 @@ func HandleUserCreate(db *gorm.DB, c *fiber.Ctx) error {
 	}
 
 	user.LastConnection = time.Now()
+	hashPwd, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println("[Users] (HandleUserCreate) - Error occurred while hashing user's password")
+	}
+	user.Password = string(hashPwd)
 	// TODO: Add entity validations and set default last_connection to null in struct
 
 	tx := db.Create(user)
 	if tx.Error != nil {
-		log.Println("[Users] (HandleUserCreate) - Error occurred while creating new user", tx.Error.Error())
+		log.Println("[Users] (HandleUserCreate) - Error occurred while writing user to the database", tx.Error.Error())
 		return c.Status(503).Send([]byte(tx.Error.Error()))
 	}
 
