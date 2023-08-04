5eb9c6f763237b1123765c435865f919b0540a98
casmeyu <martin@casamayou.net>
2023-08-03 22:07:30 -0300

    Add: user password validation regex now is an enviroment variable

--- validators/validators.go
+++ validators/validators.go
@@ -2,16 +2,19 @@ package validators
 
 import (
 	"log"
+	"os"
 	"regexp"
 
 	"github.com/go-playground/validator/v10"
 )
 
 // Custom validation for user password with a regex
-// RIGHT NOW IT DOES NOT GET CALLED
 func ValidatePasswordRegex(fl validator.FieldLevel) bool {
 	password := fl.Field().String()
-	regex := `^.{3,}$` // THIS SHOULD BE IN A ENVIRONMENT VARIABLE
+	regex, exist := os.LookupEnv("USER_PASSWORD_REGEX")
+	if exist == false {
+		regex = "^.{3,}$"
+	} // THIS SHOULD BE IN A ENVIRONMENT VARIABLE
 
 	match, err := regexp.MatchString(regex, password)
 	if err != nil {
