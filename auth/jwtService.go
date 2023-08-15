6cfec4cac6726addad8083ec6e67aa71eaab5c2e
casmeyu <martin@casamayou.net>
2023-08-15 10:08:16 -0300

    Add: JwtToken now expires according to the JWT_EXPIRATION ENV variable (in seconds)

--- auth/jwtService.go
+++ auth/jwtService.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
 func CreateJwtToken(claims map[string]interface{}) (string, error) {
+	fmt.Println("CreateJwtToken")
 	token := jwt.New(jwt.GetSigningMethod("HS256"))
 	tokenClaims := token.Claims.(jwt.MapClaims)
 
@@ -17,13 +19,13 @@ func CreateJwtToken(claims map[string]interface{}) (string, error) {
 		tokenClaims[key] = claims[key]
 	}
 	// Add expiration to JWT
-	// exp, err := strconv.Atoi(os.Getenv("JWT_EXPIRATION"))
-	// if err != nil {
-	// 	log.Println("[Auth] (CreateJwtToken) - Error occurred while getting Jwt Expiration ENV", err.Error())
-	// 	tokenClaims["exp"] = time.Hour
-	// } else {
-	tokenClaims["exp"] = time.Hour
-	// }
+	exp, err := strconv.Atoi(os.Getenv("JWT_EXPIRATION"))
+	if err != nil {
+		log.Println("[Auth] (CreateJwtToken) - Error occurred while getting Jwt Expiration ENV", err.Error())
+		tokenClaims["exp"] = time.Now().Add(time.Hour)
+	} else {
+		tokenClaims["exp"] = time.Now().Add(time.Second * time.Duration(exp))
+	}
 
 	s, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 	if err != nil {
@@ -33,25 +35,23 @@ func CreateJwtToken(claims map[string]interface{}) (string, error) {
 }
 
 func GetTokenData(tokenString string) (map[string]interface{}, error) {
-	fmt.Println("GET TOKEN DATA")
 	var data map[string]interface{}
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	})
+
 	if err != nil {
 		return nil, err
 	}
+
 	claims := token.Claims.(jwt.MapClaims)
 	if claims["data"] != nil {
 		data = claims["data"].(map[string]interface{})
 	} else {
-		data = map[string]interface{}{}
+		data = nil
 	}
 
 	return data, nil
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/go-playground/validator/v10 v10.14.1
 	github.com/gofiber/fiber/v2 v2.48.0
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.7.0
 	gorm.io/driver/mysql v1.5.1
@@ -17,7 +18,6 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
-	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
